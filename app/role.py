cdb8bf3f550d230d3d2313906d31b2825b1a58b4
ryanchanwj <59883172+ryanchanwj@users.noreply.github.com>
2022-09-28 16:54:24 +0800

    init github actions for testing and add get all roles test

--- app/role.py
+++ app/role.py
@@ -1,42 +1,71 @@
-from app import app
+# from app.skill import Skill
+# Skill = Skill
+from app.skill import Skill
+from app import app,db
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
+# db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
 
-class Role(db.Model):
-    __tablename__ = 'Role'
-    Role_ID = db.Column(db.Integer, primary_key=True)
-    Role_Name = db.Column(db.String)
-    def __init__(self, name):
-        self.name = name
+# Association Table
+Role_has_Skill = db.Table('Role_has_Skill',
+                    db.Column('Job_ID', db.Integer, db.ForeignKey('Job_Role.Job_ID')),
+                    db.Column('Skill_ID', db.Integer, db.ForeignKey('Skill.Skill_ID'))
+                    )
+
+
+
+class Job_Role(db.Model):
+    __tablename__ = 'Job_Role'
+    Job_ID = db.Column(db.Integer, primary_key=True)
+    Job_Role = db.Column(db.String)
+    Job_Title = db.Column(db.String)
+    Department = db.Column(db.String)
+    Skills = db.relationship('Skill', secondary=Role_has_Skill)
+
+    def __init__(self,Job_ID,Job_Role,Job_Title, Department, Skills):
+        self.Job_ID = Job_ID
+        self.Job_Role = Job_Role
+        self.Job_Title = Job_Title
+        self.Department = Department
+        self.Skills = Skills
    
 
     def json(self):
+        print(type(self.Skills))
         return {
-            "role_id": self.role_id,
-            "name": self.name
+            "Job_ID": self.Job_ID,
+            "Job_Role": self.Job_Role,
+            "Job_Title":self.Job_Title,
+            "Department":self.Department,
+            "Skills": [skill.jsonWithCourse() for skill in self.Skills]
         }
+        
+
 
 
 @app.route("/role/test")
 def testRole():
     return "role route is working"
 
-@app.route("/role")
+@app.route("/roles")
 def getRole():
-    roleList = Role.query.all()
+    roleList = Job_Role.query.all()
     if len(roleList):
         return jsonify(
            {
                "code": 200,
+               "error": False,
                "data": [role.json() for role in roleList]
            }
        )
     return jsonify(
         {
             "code": 200,
-            "message": "There are no roles."
+            "error": False,
+            "data": []
         }
     ), 200
+
+
 
