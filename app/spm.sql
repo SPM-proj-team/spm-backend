5ea5a57e0b63a5fc5b4fe7443d20b6fc1e7233e1
JowettC <57623021+JowettC@users.noreply.github.com>
2022-10-02 18:57:02 +0800

    database changes part 3

--- app/spm.sql
+++ app/spm.sql
@@ -73,37 +73,6 @@ INSERT INTO `Learning_Journey` (`Learning_Journey_Name`, `Staff_ID`, `Descriptio
 ('Learning Journey for Dummies', 1, 'lorem ipsum for dummies');
 COMMIT;
 
---
--- Table structure for table `Courses`
---
-
-DROP TABLE IF EXISTS `Courses`;
-CREATE TABLE IF NOT EXISTS `Courses` (
-  `Course_ID` varchar(20) NOT NULL,
-  `Course_Name` varchar(45) NOT NULL,
-  `Course_Desc` varchar(255) NOT NULL,
-  `Course_Status` varchar(15) NOT NULL,
-  `Course_Type` varchar(10) NOT NULL,
-  `Course_Category` varchar(50) NOT NULL,
-  PRIMARY KEY (`Course_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `Courses` (
-  `Course_ID`, 
-  `Course_Name`, 
-  `Course_Desc`, 
-  `Course_Status`, 
-  `Course_Type`, 
-  `Course_Category`
-  ) VALUES
-('IS212', 'Software Project Management', 'For students to learn Agile philosophies', 'Active', 'Internal', 'IT'),
-('COR3301', 'Ethics', 'For learners to gain morality', 'Active', 'External', 'Core'),
-('MGMT001', 'Management for Dummies', 'For stupid people to learn basic management', 'Active', 'Internal', 'Management'),
-('PHY001', 'Organisational Behavior', 'For managers to learn how to manage employees', 'Active', 'External', 'Management'),
-('COR1305', 'Spreadsheet Modelling', 'To learn Excel and skills like VLookup', 'Active', 'Internal', 'IT'),
-('IS412', 'Enterprise Business Solutions', 'To learn SAP products and develop real world solutions', 'Active', 'Internal', 'IT'),
-('IS211', 'Interactive Design Prototyping', 'To learn basic UI/UX skills and make wireframes', 'Active', 'Internal', 'IT');
-COMMIT;
 
 --
 -- Table structure for table `Learning_Journey_has_Course`
@@ -170,17 +139,6 @@ insert into Course values
 "Open",
 "Course_Category_1");
 
-DROP TABLE IF EXISTS `skill`;
-CREATE TABLE IF NOT EXISTS `skill` (
-  `Skill_ID` VARCHAR(13) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  PRIMARY KEY (`Skill_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `skill` (`Skill_ID`, `name`) VALUES
-('S001', 'Critical Thinking'),
-('S002', 'People Management'),
-("S003", 'Business Application');
 
 
 DROP TABLE IF EXISTS `Course_has_Skill`;
