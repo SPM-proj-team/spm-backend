cee76ec6872e05d4b8b8d485b11d4d5810f424fc
JowettC <57623021+JowettC@users.noreply.github.com>
2022-10-03 12:04:37 +0800

    dotenv and environlibrary
    removal of password and username in code level. It is now in .env file, refer to telegram chat for the file

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from dotenv import load_dotenv
 
+load_dotenv()
 
-
+user = environ.get('user') or "placeholderuser" 
+password = environ.get('password') or "placeholderpassword" 
 
 app=Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://testuser:testpass@localhost:3306/spm_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://"+user+":"+password+"@localhost:3306/spm_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 db = SQLAlchemy(app)
