8746441e21ceb3c1b32dcdc5677217f060679282
JowettC <Jowettchng@outlook.com>
2022-10-10 11:04:40 +0800

    Database URI to point to hosted database
    for consistency in data for smooth development

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,16 @@ load_dotenv()
 user = environ.get('user') or "placeholderuser" 
 password = environ.get('password') or "placeholderpassword" 
 
+AWS_DATABASE_HOST = environ.get('AWS_DATABASE_HOST') 
+AWS_DATABASE_USER = environ.get('AWS_DATABASE_USER') 
+AWS_DATABASE_PASSWORD = environ.get('AWS_DATABASE_PASSWORD') 
+AWS_DATABASE = environ.get('AWS_DATABASE') 
+
 app=Flask(__name__)
 
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://"+user+":"+password+"@localhost:3306/spm_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://"+AWS_DATABASE_USER+":"+AWS_DATABASE_PASSWORD+"@"+AWS_DATABASE_HOST+":3306/"+AWS_DATABASE
+# print
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 db = SQLAlchemy(app)
