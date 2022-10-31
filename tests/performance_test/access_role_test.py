134985dcb95169adf32b073f4cb4dca336a07c97
JowettC <jowettchng@outlook.com>
2022-10-31 21:09:26 +0800

    test

--- tests/performance_test/access_role_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def access_role():
-    url = "http://localhost:5000/accessrole"
-    response = requests.get(url)
-    return response.elapsed.total_seconds()
--- tests/performance_test/staff_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def staff():
-    url = "http://localhost:5000/staff"
-    response = requests.get(url)
-    return response.elapsed.total_seconds()
