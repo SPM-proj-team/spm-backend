881f274ada42f74e4c42b36e7bb9fc92618c7dff
ryanchanwj <ryan97chan@gmail.com>
2022-11-01 09:00:21 +0800

    add perf test to github actions

--- tests/performance_test/main.py
+++ tests/performance_test/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import time 
 import requests
-import access_role_test
-import staff_test
-import skill_test
-import course_test
-import learning_journey_test
+import access_role_perf
+import staff_perf
+import skill_perf
+import course_perf
+import learning_journey_perf
 
 df = pd.DataFrame()
 skills = []
@@ -15,27 +15,27 @@ learning_journey = []
 access_role = []
 
 # skills
-skills.append(skill_test.skill())
-skills.append(skill_test.createSkill())
-skills.append(skill_test.updateSkill())
-skills.append(skill_test.deleteSkill())
+skills.append(skill_perf.skill())
+skills.append(skill_perf.createSkill())
+skills.append(skill_perf.updateSkill())
+skills.append(skill_perf.deleteSkill())
 
 # staff
-staff.append(staff_test.staff())
+staff.append(staff_perf.staff())
 
 # course
-courses.append(course_test.course())
-courses.append(course_test.updateCourse())
+courses.append(course_perf.course())
+courses.append(course_perf.updateCourse())
 
 
 # access role
-access_role.append(access_role_test.access_role())
+access_role.append(access_role_perf.access_role())
 
 # learning journey
-learning_journey.append(learning_journey_test.learning_journey())
-learning_journey.append(learning_journey_test.createLearningJourney())
-learning_journey.append(learning_journey_test.updateLearningJourney())
-learning_journey.append(learning_journey_test.deleteLearningJourney())
+learning_journey.append(learning_journey_perf.learning_journey())
+learning_journey.append(learning_journey_perf.createLearningJourney())
+learning_journey.append(learning_journey_perf.updateLearningJourney())
+learning_journey.append(learning_journey_perf.deleteLearningJourney())
 
 df["Api Tasks"] = ["Get Skills", "Create Skill", "Update Skill", "Delete Skill", "Get Staff", "Get Courses", "Update Course", "Get Access Role", "Get Learning Journey", "Create Learning Journey", "Update Learning Journey", "Delete Learning Journey"]
 df["Time Taken"] = skills + staff + courses + access_role + learning_journey
